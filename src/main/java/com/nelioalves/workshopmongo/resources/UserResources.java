a8e44fbb03c9b43921535a6fed4f9d78c330a244
Alexandre Costa <alex.o.costa@gmail.com>
2020-05-15 15:15:10 -0300

    Usando padrão DTO para retornar usuários

--- src/main/java/com/nelioalves/workshopmongo/resources/UserResources.java
+++ src/main/java/com/nelioalves/workshopmongo/resources/UserResources.java
@@ -1,6 +1,7 @@
 package com.nelioalves.workshopmongo.resources;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.nelioalves.workshopmongo.domain.User;
+import com.nelioalves.workshopmongo.dto.UserDTO;
 import com.nelioalves.workshopmongo.services.UserService;
 
 @RestController
@@ -18,13 +20,29 @@ public class UserResources {
 	@Autowired	// o spring já faz a instanciação do objeto
 	private UserService service;
 	
+	/*
 	// @GetMapping  // forma mais simples
 	@RequestMapping(method = RequestMethod.GET)
 	public ResponseEntity<List<User>> findAll(){
-		// User maria = new User("1", "Maria Brown", "maria@gmail.com");
-		// User alex = new User("2", "Alex Silva", "alex@gmail.com");
-		// return ResponseEntity.ok().body(Arrays.asList(maria, alex));
+		User maria = new User("1", "Maria Brown", "maria@gmail.com");
+		User alex = new User("2", "Alex Silva", "alex@gmail.com");
 
+		return ResponseEntity.ok().body(Arrays.asList(maria, alex));
+	}
+	*/
+
+	/*
+	@RequestMapping(method = RequestMethod.GET)
+	public ResponseEntity<List<User>> findAll(){
 		return ResponseEntity.ok().body(service.findAll());
 	}
+	*/
+
+	@RequestMapping(method = RequestMethod.GET)
+	public ResponseEntity<List<UserDTO>> findAll(){
+		List<User> users = service.findAll();
+		List<UserDTO> usersDto = users.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(usersDto);
+	}
+
 }
