e730e607d2d3a35e6b42329a5a4979eea3971a7e
Alexandre Costa <alex.o.costa@gmail.com>
2020-05-15 11:49:59 -0300

    Conectando ao MongoDB com repository e service

--- src/main/java/com/nelioalves/workshopmongo/domain/User.java
+++ src/main/java/com/nelioalves/workshopmongo/domain/User.java
@@ -2,10 +2,16 @@ package com.nelioalves.workshopmongo.domain;
 
 import java.io.Serializable;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+// @Document	// como a coleção no mongodb tem o mesmo nome, o spring consegue mapear sem informa-lo
+@Document(collection = "user")
 public class User implements Serializable{
 
 	private static final long serialVersionUID = 1L;
 
+	@Id
 	private String id;
 	private String name;
 	private String email;
--- src/main/java/com/nelioalves/workshopmongo/resources/UserResources.java
+++ src/main/java/com/nelioalves/workshopmongo/resources/UserResources.java
@@ -1,24 +1,30 @@
 package com.nelioalves.workshopmongo.resources;
 
-import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.nelioalves.workshopmongo.domain.User;
+import com.nelioalves.workshopmongo.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResources {
 
+	@Autowired	// o spring já faz a instanciação do objeto
+	private UserService service;
+	
 	// @GetMapping  // forma mais simples
 	@RequestMapping(method = RequestMethod.GET)
 	public ResponseEntity<List<User>> findAll(){
-		User maria = new User("1", "Maria Brown", "maria@gmail.com");
-		User alex = new User("2", "Alex Silva", "alex@gmail.com");
-		return ResponseEntity.ok().body(Arrays.asList(maria, alex));
+		// User maria = new User("1", "Maria Brown", "maria@gmail.com");
+		// User alex = new User("2", "Alex Silva", "alex@gmail.com");
+		// return ResponseEntity.ok().body(Arrays.asList(maria, alex));
+
+		return ResponseEntity.ok().body(service.findAll());
 	}
 }
